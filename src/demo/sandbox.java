5a33af8d32992fad0152014219e4bd7dac5c5645
selsuean <selsuean@gmail.com>
2018-09-25 22:00:18 -0500

    first commit

---
+++ src/demo/sandbox.java
@@ -0,0 +1,22 @@
+/**
+* this is a demo java file for the sandbox repo
+*
+* 
+*
+* @author su ean lim 
+*/
+
+public class sandbox {
+	public static int firstFactorial(int n) {
+	//TODO: for input n in range 1-18, calculate and return the factorial of n	
+
+	return n;	
+	}
+
+
+	public static void main(String args[]) {
+		//user input arg[0] goes into method firstFactorial and prints onto terminal
+		System.out.println(firstFactorial(arg[0]);
+
+	}
+}
